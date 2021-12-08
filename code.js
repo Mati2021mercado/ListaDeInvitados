e3a615312f84acad7d092d15027240e97665a2e3
Matias Mercado 02 <matymercadoone@gmail.com>
2021-12-08 04:15:23 -0300

    lol

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 'use strict'
 
-fetch('./invitados.txt')
+fetch('invitados.txt')
 .then(respuesta => respuesta.text())
 .then(respuesta => respuesta.split("\r\n"))
 .then(respuesta => document.write(`<h1>En la lista hay ${respuesta.length} invitados </h1>`));
